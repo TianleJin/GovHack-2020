fd836310682232a97299f9eebc3bbed4d88db0a7
shaynedlima <dlimashayne@gmail.com>
2020-08-14 22:47:30 +1000

    Basic Flask template

---
+++ main.py
@@ -0,0 +1,16 @@
+import flask
+from flask import send_file, request, redirect, render_template, url_for, flash
+import os
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("home.html")
+    
+@app.route('/about_us')
+def about_us():
+    return render_template("about_us.html")
+
+if __name__ == '__main__':
+    app.run(port=os.getenv('PORT', 5000))
