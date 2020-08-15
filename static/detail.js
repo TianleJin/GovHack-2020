0ee3d45559da2b90998e8b68ba442aee29d971a4
TianleJin <tianlejin99@gmail.com>
2020-08-15 17:28:13 +1000

    Added event listener for map markers

--- static/detail.js
+++ static/detail.js
@@ -6,6 +6,11 @@ function initMap() {
         document.getElementById('map'), {zoom: 4, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
+    // event listener for marker
+    marker.addListener("click", () => {
+        map.setZoom(8);
+        map.setCenter(marker.getPosition());
+    });
 }
 
 var ctx = document.getElementById("radar-chart").getContext("2d")
