ffdb59112e3e6b5c66742bfa990c6ce7dd5e7e84
AbhiBalreddygari <abhi.balreddygari@outlook.com>
2020-08-15 23:04:31 +1000

    working radar chart with markers

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import flask
-from flask import send_file, request, redirect, render_template, url_for, flash
+from flask import send_file, request, redirect, render_template, url_for, flash, jsonify
 import os
 from utility import recommender as ML
+import json
 
 app = flask.Flask(__name__)
 recommender = ML.Recommender()
@@ -27,8 +28,8 @@ def handle_data():
     # Inputs: cultural diversity, age_diversity_amount
     # Output: List of 5 top recommendations (coordinates, postcode, suburb name, diversity rankings)
     lat, lng, population, suburb, cultural_diversity, age_diversity = recommender.recommend(float(cultural_diversity_amount) / 100, float(age_diversity_amount) / 100, n=5)
-
-    return render_template("detail.html", lat=lat, lng=lng, population = population, suburb = suburb, cultural_diversity = cultural_diversity, age_diversity = age_diversity)
+    print(lat, lng, population, suburb, cultural_diversity, age_diversity)
+    return render_template("detail.html", lat=lat, lng=lng, population = population, suburb = json.dumps(suburb), cultural_diversity = cultural_diversity, age_diversity = age_diversity)
 
 if __name__ == '__main__':
     app.run(port=os.getenv('PORT', 5000), debug=True)
