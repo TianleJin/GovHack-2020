137bcf6881d6f37c1c1f181287d227977226290b
shaynedlima <dlimashayne@gmail.com>
2020-08-15 20:45:13 +1000

    Attached ml recommender to maps

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import flask
 from flask import send_file, request, redirect, render_template, url_for, flash
 import os
+from utility import recommender as ML
 
 app = flask.Flask(__name__)
+recommender = ML.Recommender()
 
 @app.route('/', methods=['GET'])
 def home():
@@ -17,19 +19,11 @@ def handle_data():
     cultural_diversity_amount = request.form['cultureDiversity']
     age_diversity_amount = request.form['ageDiversity']
 
-    # your code
-    # return a response
-    print('Culture: ', cultural_diversity_amount)
-    print('Age: ', age_diversity_amount)
-
-    # ADD ML Code here
     # Inputs: cultural diversity, age_diversity_amount
     # Output: List of 5 top recommendations (coordinates, postcode, suburb name, diversity rankings)
+    postcode, lat, lng = recommender.recommend(float(cultural_diversity_amount), float(age_diversity_amount), n=5)
 
-    lat = [-37.814563, -37.81664, -37.806255, -37.837324, -37.822262]
-    lng = [144.970267, 144.987811, 144.941123, 144.976335, 144.954856]
-
-    return render_template("detail.html", lat=lat, lng=lng)
+    return render_template("detail.html", postcode=postcode, lat=lat, lng=lng)
 
 if __name__ == '__main__':
     app.run(port=os.getenv('PORT', 5000), debug=True)
